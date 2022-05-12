6a94aa768518019219d05a9ec45b51eee2734a8e
Hari Ganesh Varma Mulagapati <105410021+HariGaneshVarma-Mulagapati@users.noreply.github.com>
2022-05-12 12:16:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ IMPORT STREAMLIT
 
 STREAMLIT.TITLE('My Parents New Healthy Diner')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
